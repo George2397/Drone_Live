bf3b34233c39cf091bd5c06437ea353b95d048b8
George2397 <sofronioug@gmail.com>
2020-07-29 17:06:45 +0300

    Added motion to the 3D model

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //Make the video element draggagle:
-console.log("here")
 var videoFrame = document.getElementById("mydiv");
 
 if (videoFrame)
@@ -8,7 +7,7 @@ if (videoFrame)
 }
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2VvcmdlMjMyMyIsImEiOiJja2MwZmxjbGYxajF4MnJsZ2pzbjhjdHc2In0.znh7LExrIEsKBB7SWYJ3hg';
-const {MapboxLayer, PointCloudLayer} = deck;
+const {MapboxLayer, PointCloudLayer, LineLayer} = deck;
 
 var map = new mapboxgl.Map({
     container: 'map',
@@ -16,6 +15,7 @@ var map = new mapboxgl.Map({
     pitch: 60,
     zoom: 17.5,
     center: [ 33.4151176797,35.1452954125]
+    // center: [148.9819, -35.39847]
 });
 
 // Add zoom and rotation controls to the map.
@@ -77,8 +77,8 @@ var wholeTripCircleLayer =
     }
 
 //following url contains only one feature which is updated every ~100ms
-// var url = 'http://localhost:63340/Drone_Live/pythonLocUpdate_Script/aiders.geojson'; //Works as well
-var url = 'pythonLocUpdate_Script/aiders.geojson';
+var url = 'http://localhost:63340/Drone_Live/pythonLocUpdate_Script/aiders.geojson'; //Works as well
+// var url = 'pythonLocUpdate_Script/aiders.geojson';
 
 //This object is required to display the yellow trail of the drone
 var droneTrailObject =
@@ -98,7 +98,7 @@ var droneTrailObject =
         ]
     };
 
-var trailLayerStyle =
+var trailLayerData =
     {
         "id": "trailLayerStyle",
         "type": "line",
@@ -111,6 +111,113 @@ var trailLayerStyle =
             }
     };
 
+//=====================================================================================================================
+{
+    // parameters to ensure the model is georeferenced correctly on the map
+    var modelOrigin = [33.4151176797, 35.1452954125];
+    var modelAltitude = 0;
+    var modelRotate = [Math.PI / 2, 0, 0];
+
+    var modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
+        modelOrigin,
+        modelAltitude
+    );
+
+// transformation parameters to position, rotate and scale the 3D model onto the map
+    var modelTransform = {
+        translateX: modelAsMercatorCoordinate.x,
+        translateY: modelAsMercatorCoordinate.y,
+        translateZ: modelAsMercatorCoordinate.z,
+        rotateX: modelRotate[0],
+        rotateY: modelRotate[1],
+        rotateZ: modelRotate[2],
+        /* Since our 3D model is in real world meters, a scale transform needs to be
+         * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+         */
+        scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
+    };
+
+    var THREE = window.THREE;
+
+// configuration of the custom layer for a 3D model per the CustomLayerInterface
+    var customLayer = {
+        id: '3d-model',
+        type: 'custom',
+        renderingMode: '3d',
+        onAdd: function(map, gl) {
+            this.camera = new THREE.Camera();
+            this.scene = new THREE.Scene();
+
+            // create two three.js lights to illuminate the model
+            var directionalLight = new THREE.DirectionalLight(0xffffff);
+            directionalLight.position.set(0, -70, 100).normalize();
+            this.scene.add(directionalLight);
+
+            var directionalLight2 = new THREE.DirectionalLight(0xffffff);
+            directionalLight2.position.set(0, 70, 100).normalize();
+            this.scene.add(directionalLight2);
+
+            // use the three.js GLTF loader to add the 3D model to the three.js scene
+            var loader = new THREE.GLTFLoader();
+            loader.load(
+                'https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf',
+                // 'random_threeD.gltf',
+                function(gltf) {
+                    this.scene.add(gltf.scene);
+                }.bind(this)
+            );
+            this.map = map;
+
+            // use the Mapbox GL JS map canvas for three.js
+            this.renderer = new THREE.WebGLRenderer({
+                canvas: map.getCanvas(),
+                context: gl,
+                antialias: true
+            });
+
+            this.renderer.autoClear = false;
+        },
+        render: function(gl, matrix) {
+            var rotationX = new THREE.Matrix4().makeRotationAxis(
+                new THREE.Vector3(1, 0, 0),
+                modelTransform.rotateX
+            );
+            var rotationY = new THREE.Matrix4().makeRotationAxis(
+                new THREE.Vector3(0, 1, 0),
+                modelTransform.rotateY
+            );
+            var rotationZ = new THREE.Matrix4().makeRotationAxis(
+                new THREE.Vector3(0, 0, 1),
+                modelTransform.rotateZ
+            );
+
+            var m = new THREE.Matrix4().fromArray(matrix);
+            var l = new THREE.Matrix4()
+                .makeTranslation(
+                    modelTransform.translateX,
+                    modelTransform.translateY,
+                    modelTransform.translateZ
+                )
+                .scale(
+                    new THREE.Vector3(
+                        modelTransform.scale,
+                        -modelTransform.scale,
+                        modelTransform.scale
+                    )
+                )
+                .multiply(rotationX)
+                .multiply(rotationY)
+                .multiply(rotationZ);
+
+            this.camera.projectionMatrix = m.multiply(l);
+            this.renderer.state.reset();
+            this.renderer.render(this.scene, this.camera);
+            this.map.triggerRepaint();
+        }
+    };
+
+}
+//=====================================================================================================================
 //This is a mapbox layer that adds height to the buildings
 var threeDlayer =
     {
@@ -141,7 +248,23 @@ var pointCloudLayer = new MapboxLayer(
     }
 );
 
-map.on('load', function ()
+// Create a mapbox-compatible deck.gl layer
+const myDeckLayer = new MapboxLayer({
+    id: 'my-scatterplot',
+    type: ScatterplotLayer,
+    data: [
+        {position: [100.434,30.3344, 10], size: 3}
+
+    ],
+    getPosition: d => d.position,
+    getRadius: d => d.size,
+    getColor: [255, 0, 0]
+});
+
+
+
+
+map.on('style.load', function ()
 {
     var i = 0;
     var updateInterval = 100; //milliseconds
@@ -154,6 +277,14 @@ map.on('load', function ()
             var currentCoordinate = geojson.geometry.coordinates;
             var currentBatteryLevel = geojson.properties.batttery
             var currentFeature = geojson;
+
+            var currentCoordinateObject =
+                {
+                    position: currentCoordinate,
+                    size: 5
+                };
+
+            myDeckLayer.props.data.push(currentCoordinateObject)
             droneTrailObject.features[0].geometry.coordinates.push(currentCoordinate)
 
             if (currentBatteryLevel > 80)
@@ -169,19 +300,23 @@ map.on('load', function ()
             popup.setLngLat(currentCoordinate);
             marker.setLngLat(currentCoordinate);
 
+            moveThreeDElement(currentCoordinate,i);
             if (i === 100) //Random point to stop
             {
-                clearMap(trailLayerStyle, marker, droneTimer, wholeTripCircleLayer)
+                clearMap(trailLayerData, marker, droneTimer, wholeTripCircleLayer)
             }
             i++;
         });
     }
 
     map.addSource('trajectory', {type: 'geojson', data: droneTrailObject});
-    map.addLayer(trailLayerStyle);
+    // map.addSource('my-scatterplot', {type: 'geojson', data: myDeckLayer});
+    map.addLayer(trailLayerData);
     map.addLayer(wholeTripCircleLayer)
     map.addLayer(threeDlayer, 'waterway-label')
     map.addLayer(pointCloudLayer, 'waterway-label')
+    map.addLayer(myDeckLayer, 'waterway-label');
+    map.addLayer(customLayer, 'waterway-label');
 });
 
 /*
@@ -234,4 +369,20 @@ function dragElement(elmnt) {
         document.onmouseup = null;
         document.onmousemove = null;
     }
+}
+
+function moveThreeDElement(currentCoordinate, i)
+{
+    modelOrigin = [currentCoordinate[0], currentCoordinate[1]];
+    // modelAltitude = currentCoordinate[2];
+    modelAltitude = i; //Giving the repetition as an altitude to check if it works
+
+    modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
+        modelOrigin,
+        modelAltitude
+    );
+
+    modelTransform.translateX = modelAsMercatorCoordinate.x;
+    modelTransform.translateY = modelAsMercatorCoordinate.y;
+    modelTransform.translateZ = modelAsMercatorCoordinate.z;
 }
